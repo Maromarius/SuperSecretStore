575e7e03bf5d69f52c37b4b87212b532f4e26566
Santiago <sfinoli@yahoo.com>
2013-11-13 01:51:11 -0500

    DB query testing & schema manipulation UI.

--- GameStoreInventory/src/com/gamestoreinventory/util/Driver.java
+++ GameStoreInventory/src/com/gamestoreinventory/util/Driver.java
@@ -1,35 +1,50 @@
+/*
+ * Run as a Java application if you want to test queries 
+ * or manipulate database schema.
+ */
 package com.gamestoreinventory.util;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Driver 
 {
-	public static void main(String[] args) throws SQLException 
+	public static void main(String[] args)
 	{
 		Connection connection = DatabaseConnection.getConnection();
 
 		if (connection != null) 
 		{
-			System.out.println("Connection : true");
-			
-			Statement statement = null;
-			try 
+			System.out.println("Database connection success!");
+			Scanner keyboard = new Scanner(System.in);
+			String sql = null;
+			while (true)
 			{
-				statement = connection.createStatement();
-			} catch (SQLException e) 
-			{
-				e.printStackTrace();
+				System.out.println("\nEnter your query ('$' to exit): ");
+				sql = keyboard.nextLine().trim();
+				
+				if (sql.equals("$"))
+					break;
+				
+				try 
+				{
+					Statement statement = connection.createStatement();
+					statement.execute(sql);
+					System.out.println("Your SQL statement was successfully processed.");
+				} catch (Exception e)
+				{
+					System.out.println("Error encountered while processing your statement. Please try again.");
+				}
 			}
-			String sql = "Select * From Person";
-			System.out.println(statement.execute(sql));
-			
 			DatabaseConnection.close(null, null, connection);
+			System.out.println("Exiting...");
+			keyboard.close();
+			System.exit(0);
 		} 
 		else 
 		{
-			System.out.println("Connection : false");
+			System.out.println("Database connection failed!");
 		}
 	}
 }
